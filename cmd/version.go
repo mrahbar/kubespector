a94e6dc70af28b2804d821cb2be26254921454f9
Mahmoud Reza Rahbar Azad <mahmoud.azad@acando.de>
2017-03-06 08:16:42 +0100

    initial commit

---
+++ cmd/version.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	VERSION = "0.1.0"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints the current version of kubernetes-inspector",
+	Long: `The version is aligned with the SemVer specification, e.q. 1.0.0`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(RootCmd.Use + " version " + VERSION)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/mrahbar/kubernetes-inspector/cmd"
+
+func main() {
+	cmd.Execute()
+}
