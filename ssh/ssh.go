7a88312b1d8900bf77fd2636ba1d7a1fdfab8b6b
Mahmoud Reza Rahbar Azad <mahmoud.azad@acando.de>
2017-08-23 21:59:56 +0200

    fixed trailing space in single cmd shell

--- ssh/ssh.go
+++ ssh/ssh.go
@@ -10,17 +10,20 @@ import (
 	"github.com/mrahbar/kubernetes-inspector/ssh/communicator"
 	"github.com/mrahbar/kubernetes-inspector/types"
 	"github.com/mrahbar/kubernetes-inspector/util"
+	"golang.org/x/crypto/ssh"
 )
 
 func PerformCmd(sshOpts types.SSHConfig, node types.Node, cmd string, debug bool) (*types.SSHOutput, error) {
 	if util.NodeEquals(sshOpts.LocalOn, node) {
 		splits := strings.SplitN(cmd, " ", 1)
-		args := ""
+		var args []string
 		if len(splits) > 1 {
-			args = splits[1]
+			args = strings.Split(splits[1], " ")
+		} else {
+			args = []string{}
 		}
-		out, err := shell(splits[0], debug, args)
-		return &types.SSHOutput{Stdout: out}, err
+		out, err := shell(splits[0], debug, args...)
+		return out, err
 	}
 
 	comm, err := establishSSHCommunication(sshOpts, util.GetNodeAddress(node), debug)
@@ -64,19 +67,36 @@ func PerformCmd(sshOpts types.SSHConfig, node types.Node, cmd string, debug bool
 }
 
 // Shell runs the command, binding Stdin, Stdout and Stderr
-func shell(binaryPath string, debug bool, args ...string) (string, error) {
+func shell(binaryPath string, debug bool, args ...string) (*types.SSHOutput, error) {
 	cmd := exec.Command(binaryPath, args...)
 	if debug {
 		cmdDebug := append([]string{}, cmd.Args...)
 		fmt.Printf("Executing command: %s\n", cmdDebug)
 	}
 
+	var stderr bytes.Buffer
 	cmd.Stdin = os.Stdin
-	o, err := cmd.CombinedOutput()
-	output := strings.TrimSpace(string(o))
-	if debug {
-		fmt.Printf("Result of command:\nResult: %sErr: %s\n", output, err)
+	cmd.Stderr = &stderr
+	out, err := cmd.Output()
+	output := strings.TrimSpace(string(out))
+	outErr := strings.TrimSpace(stderr.String())
+	o := &types.SSHOutput{Stdout: output, Stderr: outErr}
+
+	exitStatus := 0
+	if err != nil {
+		if _, ok := err.(*exec.ExitError); ok {
+			exitStatus = err.(*ssh.ExitError).ExitStatus()
+		}
 	}
 
-	return output, err
+	if debug {
+		errFormatted := ""
+		if err != nil {
+			errFormatted = fmt.Sprintf("%s", err)
+		}
+		util.PrettyPrintDebug("Result of command:\nStdout: %s\nStderr: %s\nExitStatus: %d\nErr: %s\n",
+			output, outErr, exitStatus, errFormatted)
+	}
+
+	return o, err
 }
